9ea5002b441ccde1e1fe4e5db1464c68ead7a1cb
Juan Fernando Villa Hernández <elite.linux@gmail.com>
2015-01-09 09:02:01 -0500

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+{
+   "lang":"en",
+   "url":"http://www.openculture.com",
+   "info":"List of free online courses is broken down by subject matter and includes classes available on YouTube, iTunes U, and direct from the University or School's website."
+   "certificate":"not applicable"
+   "type":"Directory"
+   
+}
